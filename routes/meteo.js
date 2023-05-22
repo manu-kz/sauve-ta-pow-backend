f122ec8a461a8eb343612ae28b84d892f81178be
Emmanuel Kurtz <128033919+manu-kz@users.noreply.github.com>
2023-05-22 18:58:48 +0200

    ajout des infos BRA pour tous les massifs

--- routes/meteo.js
+++ routes/meteo.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+const parseString = require('xml2js').parseString;
+
 const fetch = require('node-fetch');
+
+
 
 const apiKey = process.env.METEO_API_KEY;
 
@@ -24,10 +28,9 @@ router.get('/location/:location', (req, res) => {
     });
 });
 
-
 // GET Current conditions according to location key
 
-router.get('/:locationKey', (req, res) => {
+router.get('/locationKey/:locationKey', (req, res) => {
   const locationKey = req.params.locationKey;
 
   fetch(
@@ -44,43 +47,88 @@ router.get('/:locationKey', (req, res) => {
     });
 });
 
-
-// GET hourly according to location key
+// GET hourly weather according to location key
 
 router.get('/hourly/:locationKey', (req, res) => {
   const locationKey = req.params.locationKey;
 
   fetch(
- `http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/${locationKey}?apikey=${apiKey}&language=fr-fr&metric=true`
+    `http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/${locationKey}?apikey=${apiKey}&language=fr-fr&metric=true`
   )
     .then((response) => response.json())
     .then((data) => {
       if (data) {
         // return données de meteo du moment
-       res.json({ meteo: data });
+        res.json({ meteo: data });
       } else {
         res.json({ error: 'Location not found' });
       }
     });
 });
+
+// GET daily weather according to location key
 
 router.get('/daily/:locationKey', (req, res) => {
   const locationKey = req.params.locationKey;
 
   fetch(
- `http://dataservice.accuweather.com/forecasts/v1/daily/5day/${locationKey}?apikey=${apiKey}&language=fr-fr&metric=true`
+    `http://dataservice.accuweather.com/forecasts/v1/daily/5day/${locationKey}?apikey=${apiKey}&language=fr-fr&metric=true`
   )
     .then((response) => response.json())
     .then((data) => {
       if (data) {
         // return données de meteo du moment
-       res.json({ meteo: data.DailyForecasts });
+        res.json({ meteo: data.DailyForecasts });
       } else {
         res.json({ error: 'Location not found' });
       }
     });
 });
 
+// GET total daily BRA 
+router.get('/bra/', (req, res) => {
 
+  let allBra = []
+
+  for (let i=1; i<24; i++){
+    let numBra = i > 9 ? i : `0${i}`;
+
+  let promise = fetch(`http://api.meteofrance.com/files/mountain/bulletins/BRA${numBra}.xml`)
+  .then(response => response.text())
+  .then(responseText => {
+      return new Promise((resolve, reject) => {
+        parseString(responseText, function(err, result) {
+          if (err) reject(err);
+          else resolve(result);
+        });
+      });
+    })
+    .catch((err) => console.log(err));
+
+    allBra.push(promise);
+  }
+
+  Promise.all(allBra)
+    .then(bra => res.json({ bra }))
+    .catch((err) => res.json({ error: 'BRA not found' }));
+});
+
+
+// GET unique BRA
+router.get('/bra/chablais', (req, res) => {
+
+  fetch(`http://api.meteofrance.com/files/mountain/bulletins/BRA01.xml`)
+  .then(response => response.text())
+  .then(responseText => {
+const xmlData = responseText
+parseString(xmlData, function(err, result){
+  if (result) {
+    res.json({ bra : result});
+  } else {
+    res.json({ error: 'BRA not found' });
+  }
+})
+   });
+});
 
 module.exports = router;
